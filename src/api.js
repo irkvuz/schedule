be1bd0dd6d6d7ae8aee00ee024bb514a2daecdd3
Yury Savin <savinyurii@gmail.com>
2018-11-05 18:17:43 +0800

    Start api wrapper

---
+++ src/api.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+axios.defaults.baseURL = 'http://mobile.bgu.ru/timetableJson.ashx';
+
+function getFaculties() {
+  return axios.get(`/`);
+}
+
+module.exports = { getFaculties }
+
+// simple testing
+if (require.main === module) {
+  (async () => {
+    let api = module.exports;
+    // api.getFaculties().then(res => console.log(res)).catch(err => console.error(err))
+    let res;
+    try {
+      res = await api.getFaculties();
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(JSON.parse(res.data.substr(1)))
+  })();
+}
