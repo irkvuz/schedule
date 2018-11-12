ee22c9613b500bfb9248a3f6eb1e53b2492570eb
Yury Savin <savinyurii@gmail.com>
2018-11-12 10:43:03 +0300

    Add LocaleProvider

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import TimeTable from './TimeTable/TimeTable';
 import { ListFaculties, ListGroups } from './Lists';
+import { LocaleProvider } from 'antd';
+import ru_RU from 'antd/lib/locale-provider/ru_RU';
+import 'moment/locale/ru';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Switch>
-            <Route
-              path="/:facultyId(\d+)/:groupId(\d+)"
-              component={TimeTable}
-            />
-            <Route path="/:facultyId(\d+)" component={ListGroups} />
-            <Route path="/" component={ListFaculties} />
-          </Switch>
-        </div>
-      </Router>
+      <LocaleProvider locale={ru_RU}>
+        <Router>
+          <div>
+            <Link to="/">Home</Link>
+            <Switch>
+              <Route
+                path="/:facultyId(\d+)/:groupId(\d+)"
+                component={TimeTable}
+              />
+              <Route path="/:facultyId(\d+)" component={ListGroups} />
+              <Route path="/" component={ListFaculties} />
+            </Switch>
+          </div>
+        </Router>
+      </LocaleProvider>
     );
   }
 }
