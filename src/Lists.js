f7ccea08c054db02f57fc321dfcfb4f0f7c53242
Yury Savin <savinyurii@gmail.com>
2018-12-17 09:51:28 +0300

    Add search

--- src/Lists.js
+++ src/Lists.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import List from 'antd/lib/list';
+import Input from 'antd/lib/input';
 
 const api = require('./api');
+
+/**
+ * Used for ListFaculties and ListGroups
+ *
+ * @param {*} props
+ * @returns
+ */
+class UniversalList extends React.Component {
+  state = {
+    searchValue: '',
+  };
+  handleSearch = e => {
+    const searchValue = e.target.value;
+    this.setState({ searchValue });
+  };
+  render() {
+    const { title, dataSource, loading, fieldNames } = this.props;
+    const filtredDataSource = dataSource.filter(i =>
+      i[fieldNames.label].match(new RegExp(this.state.searchValue, 'i'))
+    );
+    return (
+      <div>
+        <h2>{title}</h2>
+        <Input.Search onChange={this.handleSearch} />
+        <br />
+        <br />
+        <List
+          dataSource={filtredDataSource}
+          loading={loading}
+          bordered
+          renderItem={i => (
+            <List.Item key={i[fieldNames.value]}>
+              <Link to={`${i[fieldNames.value]}/`}>{i[fieldNames.label]}</Link>
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 class ListFaculties extends React.Component {
   constructor(props) {
@@ -25,19 +66,12 @@ class ListFaculties extends React.Component {
   };
   render() {
     return (
-      <div>
-        <h2>Список факультетов</h2>
-        <List
-          dataSource={this.state.faculties}
-          loading={this.state.loading}
-          bordered
-          renderItem={f => (
-            <List.Item key={f.IdFaculty}>
-              <Link to={`${f.IdFaculty}/`}>{f.FacultyName}</Link>
-            </List.Item>
-          )}
-        />
-      </div>
+      <UniversalList
+        title="Список факультетов"
+        dataSource={this.state.faculties}
+        loading={this.state.loading}
+        fieldNames={{ label: 'FacultyName', value: 'IdFaculty' }}
+      />
     );
   }
 }
@@ -64,19 +98,12 @@ class ListGroups extends React.Component {
   render() {
     console.log(this.state.groups);
     return (
-      <div>
-        <h2>Список групп</h2>
-        <List
-          dataSource={this.state.groups}
-          loading={this.state.loading}
-          bordered
-          renderItem={g => (
-            <List.Item key={g.IdGroup}>
-              <Link to={`${g.IdGroup}/`}>{g.Group}</Link>
-            </List.Item>
-          )}
-        />
-      </div>
+      <UniversalList
+        title="Список групп"
+        dataSource={this.state.groups}
+        loading={this.state.loading}
+        fieldNames={{ label: 'Group', value: 'IdGroup' }}
+      />
     );
   }
 }
