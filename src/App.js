1ce6d85ac999c8cb3d7f5b556778a54eb180abe0
Yury Savin <savinyurii@gmail.com>
2018-12-18 17:49:31 +0800

    📈 Use yandex metrika

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
 import Schedule from './Schedule/Schedule';
 import { ListFaculties, ListGroups } from './Lists';
 import { LocaleProvider, Icon } from 'antd';
+import { YMInitializer } from 'react-yandex-metrika';
 import ru_RU from 'antd/lib/locale-provider/ru_RU';
 import 'moment/locale/ru';
 
@@ -11,38 +12,52 @@ class App extends React.Component {
     console.log('App component rendered');
 
     return (
-      <LocaleProvider locale={ru_RU}>
-        <BrowserRouter>
-          <>
-            <header>
-              <Link to="/faculties" title="Изменить группу">
-                <Icon type="home" />
-              </Link>
-            </header>
+      <>
+        <LocaleProvider locale={ru_RU}>
+          <BrowserRouter>
+            <>
+              <header>
+                <Link to="/faculties" title="Изменить группу">
+                  <Icon type="home" />
+                </Link>
+              </header>
 
-            <Switch>
-              <Route
-                path="/:facultyId(\d+)/:groupId(\d+)"
-                component={Schedule}
-              />
-              <Route path="/:facultyId(\d+)" component={ListGroups} />
-              <Route path="/faculties" component={ListFaculties} />
-              <Route
-                path="/"
-                render={() => {
-                  let facultyId = localStorage['facultyId'],
-                    groupId = localStorage['groupId'];
-                  if (facultyId && groupId)
-                    return <Redirect to={`/${facultyId}/${groupId}`} />;
-                  else return <Redirect to="/faculties" />;
-                }}
-              />
-            </Switch>
+              <Switch>
+                <Route
+                  path="/:facultyId(\d+)/:groupId(\d+)"
+                  component={Schedule}
+                />
+                <Route path="/:facultyId(\d+)" component={ListGroups} />
+                <Route path="/faculties" component={ListFaculties} />
+                <Route
+                  path="/"
+                  render={() => {
+                    let facultyId = localStorage['facultyId'],
+                      groupId = localStorage['groupId'];
+                    if (facultyId && groupId)
+                      return <Redirect to={`/${facultyId}/${groupId}`} />;
+                    else return <Redirect to="/faculties" />;
+                  }}
+                />
+              </Switch>
 
-            <footer>© Yury Savin, Irkutsk 2018</footer>
-          </>
-        </BrowserRouter>
-      </LocaleProvider>
+              <footer>© Yury Savin, Irkutsk 2018</footer>
+            </>
+          </BrowserRouter>
+        </LocaleProvider>
+        {process.env.NODE_ENV === 'production' && (
+          <YMInitializer
+            accounts={[50381566]}
+            options={{
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true,
+            }}
+            version="2"
+          />
+        )}
+      </>
     );
   }
 }
