0ddcb7f77a86dd707b90a84212fbe86d6273f416
Yury Savin <savinyurii@gmail.com>
2019-01-15 23:36:04 +0800

    Add types to api

--- src/api.tsx
+++ src/api.tsx
@@ -3,9 +3,9 @@ import axios from 'axios';
 let api = {
   getFaculties: () => axios.get(`/data/faculties.json`),
   getFacultiesWithGroups: () => axios.get(`/data/facultiesWithGroups.json`),
-  getGroups: facultyId => axios.get(`/data/groups/${facultyId}.json`),
+  getGroups: (facultyId:number) => axios.get(`/data/groups/${facultyId}.json`),
   getTrimester: () => axios.get(`/data/trimesters/current.json`),
-  getSchedule: (groupId, trimesterId) =>
+  getSchedule: (groupId:number, trimesterId:number) =>
     axios.get(`/data/schedule/${trimesterId}/${groupId}.json`),
 };
 
