f5b444f9c043cfeb84339270b3c8a2bed0b72787
Yury Savin <savinyurii@gmail.com>
2019-01-22 01:37:10 +0800

    Add script for CSV

---
+++ scripts/saveAsCSV.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = 'public/data/schedule/1079/';
+
+const files = fs.readdirSync(dir);
+
+const separator = '\t';
+const columns = [
+  'group',
+  'WeekDay',
+  'StartTime',
+  'Odd',
+  'Room',
+  'LessonType',
+  'FIO',
+  'Lesson',
+];
+let csv = columns.join(separator) + '\n';
+
+files.forEach((file, index) => {
+  const schedule = JSON.parse(
+    fs.readFileSync(dir + file, { encoding: 'utf-8' })
+  );
+
+  schedule.forEach((l, index) => {
+    const row = [
+      file.split('.')[0],
+      l.WeekDay,
+      l.StartTime,
+      l.Odd,
+      l.Room,
+      l.LessonType,
+      l.FIO,
+      l.Lesson,
+    ];
+    csv += row.join(separator) + `\n`;
+  });
+});
+
+fs.writeFileSync('schedule.csv', csv);
