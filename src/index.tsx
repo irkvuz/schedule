fd2e54382f7d7344940e23124a6b079f83b86f52
Yury Savin <savinyurii@gmail.com>
2019-01-22 03:08:13 +0800

    📱 Enable ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { message } from 'antd';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const config = {
+  onSuccess: (registration: ServiceWorkerRegistration) => {
+    message.success('Offline ready', 1);
+  },
+  onUpdate: (registration: ServiceWorkerRegistration) => {
+    message.info('Доступно обновление');
+  },
+};
+serviceWorker.register(config);
