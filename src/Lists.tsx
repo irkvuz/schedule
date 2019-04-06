a062052ed3cf901f7ec83cf35c6344d97a29ccc3
Yury Savin <savinyurii@gmail.com>
2019-04-06 16:12:31 +0300

    🎉 Translate UniversalList to finctional component

--- src/Lists.tsx
+++ src/Lists.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import List from 'antd/lib/list';
 import Input from 'antd/lib/input';
@@ -7,48 +7,43 @@ import api from './api';
 /**
  * Used for ListFaculties and ListGroups
  */
-class UniversalList extends React.Component<any, any> {
-  state = {
-    searchValue: '',
-  };
-  handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+function UniversalList({ title, dataSource, loading, fieldNames }: any) {
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchValue = event.target.value;
-    this.setState({ searchValue });
+    setSearchValue(searchValue);
   };
-  render() {
-    const { title, dataSource, loading, fieldNames } = this.props;
-    const filtredDataSource = dataSource.filter((item: any) =>
-      item[fieldNames.label].match(new RegExp(this.state.searchValue, 'i'))
-    );
-    return (
-      <>
-        <h2>{title}</h2>
-        <Input.Search
-          onChange={this.handleSearch}
-          placeholder="Начните вводить название..."
-        />
-        {/* @TODO remove br, use css instead */}
-        <br />
-        <br />
-        <List
-          dataSource={filtredDataSource}
-          loading={loading}
-          bordered
-          renderItem={(item: any) => (
-            <List.Item key={item[fieldNames.value]}>
-              {item.hasSchedule || !item.hasOwnProperty('hasSchedule') ? (
-                <Link to={`${item[fieldNames.value]}/`}>
-                  {item[fieldNames.label]}
-                </Link>
-              ) : (
-                <span style={{ opacity: 0.5 }}>{item[fieldNames.label]}</span>
-              )}
-            </List.Item>
-          )}
-        />
-      </>
-    );
-  }
+  const filtredDataSource = dataSource.filter((item: any) =>
+    item[fieldNames.label].match(new RegExp(searchValue, 'i'))
+  );
+  return (
+    <>
+      <h2>{title}</h2>
+      <Input.Search
+        onChange={handleSearch}
+        placeholder="Начните вводить название..."
+      />
+      {/* @TODO remove br, use css instead */}
+      <br />
+      <br />
+      <List
+        dataSource={filtredDataSource}
+        loading={loading}
+        bordered
+        renderItem={(item: any) => (
+          <List.Item key={item[fieldNames.value]}>
+            {item.hasSchedule || !item.hasOwnProperty('hasSchedule') ? (
+              <Link to={`${item[fieldNames.value]}/`}>
+                {item[fieldNames.label]}
+              </Link>
+            ) : (
+              <span style={{ opacity: 0.5 }}>{item[fieldNames.label]}</span>
+            )}
+          </List.Item>
+        )}
+      />
+    </>
+  );
 }
 
 interface MatchParams {
