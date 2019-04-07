8782e8def79805ef7d83f677e81e1ba0b06adc6c
Yury Savin <savinyurii@gmail.com>
2019-04-08 00:34:37 +0300

    Make abstract get function

--- src/api.tsx
+++ src/api.tsx
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import { ITrimester, ILessonOld } from './constants';
 
+/** Wrapper for `axios.get` that returns `response`.data */
+async function get<T = any>(url: string) {
+  const response = await axios.get<T>(url);
+  return response.data;
+}
+
 let api = {
   getFaculties: () => axios.get(`/data/faculties.json`),
   getFacultiesWithGroups: () => axios.get(`/data/facultiesWithGroups.json`),
   getGroups: (facultyId: string) => axios.get(`/data/groups/${facultyId}.json`),
-  getTrimester: () =>
-    axios
-      .get<ITrimester>(`/data/trimesters/current.json`)
-      .then(res => res.data),
+  getTrimester: () => get<ITrimester>(`/data/trimesters/current.json`),
   getSchedule: (groupId: string, trimesterId: string | number) =>
-    axios
-      .get<ILessonOld[]>(`/data/schedule/${trimesterId}/${groupId}.json`)
-      .then(res => res.data),
+    get<ILessonOld[]>(`/data/schedule/${trimesterId}/${groupId}.json`),
 };
 
 export default api;
