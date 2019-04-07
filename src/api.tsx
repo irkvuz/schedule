c62155b93f1eeec663a1603cb5725a61064d3ebc
Yury Savin <savinyurii@gmail.com>
2019-04-08 00:26:25 +0300

    Specify types returned by API

--- src/api.tsx
+++ src/api.tsx
@@ -1,12 +1,18 @@
 import axios from 'axios';
+import { ITrimester, ILessonOld } from './constants';
 
 let api = {
   getFaculties: () => axios.get(`/data/faculties.json`),
   getFacultiesWithGroups: () => axios.get(`/data/facultiesWithGroups.json`),
   getGroups: (facultyId: string) => axios.get(`/data/groups/${facultyId}.json`),
-  getTrimester: () => axios.get(`/data/trimesters/current.json`),
-  getSchedule: (groupId: string, trimesterId: string) =>
-    axios.get(`/data/schedule/${trimesterId}/${groupId}.json`),
+  getTrimester: () =>
+    axios
+      .get<ITrimester>(`/data/trimesters/current.json`)
+      .then(res => res.data),
+  getSchedule: (groupId: string, trimesterId: string | number) =>
+    axios
+      .get<ILessonOld[]>(`/data/schedule/${trimesterId}/${groupId}.json`)
+      .then(res => res.data),
 };
 
 export default api;
