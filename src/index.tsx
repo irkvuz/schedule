0c79fd72005d49639426b796c5233367b58a82d5
Yury Savin <yury@savin.dev>
2019-07-30 10:36:56 +0300

    📴 Disable serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -10,18 +10,18 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-const config = {
-  onSuccess: (registration: ServiceWorkerRegistration) => {
-    message.success('Offline ready', 1);
-  },
-  onUpdate: (registration: ServiceWorkerRegistration) => {
-    notification.success({
-      message: 'Доступно обновление',
-      description: 'Нажмите на это уведомление, чтобы обновить приложение',
-      placement: 'bottomRight',
-      onClick: () => window.location.reload(true),
-    });
-  },
-};
-serviceWorker.register(config);
-// serviceWorker.unregister();
+// const config = {
+//   onSuccess: (registration: ServiceWorkerRegistration) => {
+//     message.success('Offline ready', 1);
+//   },
+//   onUpdate: (registration: ServiceWorkerRegistration) => {
+//     notification.success({
+//       message: 'Доступно обновление',
+//       description: 'Нажмите на это уведомление, чтобы обновить приложение',
+//       placement: 'bottomRight',
+//       onClick: () => window.location.reload(true),
+//     });
+//   },
+// };
+// serviceWorker.register(config);
+serviceWorker.unregister();
