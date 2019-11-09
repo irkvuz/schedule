42837a438213e4a81a156340fdc993c2fda057a9
Yury Savin <yury@savin.dev>
2019-11-09 17:24:41 +0100

    Add babel-plugin-import

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,9 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-};
+const { override, fixBabelImports } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  })
+);
