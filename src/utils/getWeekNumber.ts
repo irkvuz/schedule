7c93b605f1d5850369be7687e3d7f1d93c3fa291
Yury Savin <yury@savin.dev>
2020-02-22 21:06:57 +0100

    Migrate getWeekNumber to date-fns

--- src/utils/getWeekNumber.ts
+++ src/utils/getWeekNumber.ts
@@ -1,13 +1,15 @@
-import moment from 'moment';
+import { differenceInCalendarISOWeeks, parseJSON } from 'date-fns';
 /**
  * Returns current week number in trimester, from `dateStart`
  * @param sDateStart strart of trimester, e.g. `2018-09-01T00:00:00`
  * @param sToday only for testing
  */
 export function getWeekNumber(sDateStart: string, sToday?: string): number {
-  const dateStart = moment(sDateStart);
-  const today = sToday ? moment(sToday) : moment();
+  const dateStart = parseJSON(sDateStart);
+  const today = sToday ? new Date(sToday) : new Date();
+  // this function assume that first day of week is 1 - Monday
+  const diff = differenceInCalendarISOWeeks(today, dateStart);
 
-  const startOfWeek = dateStart.startOf('week');
-  return moment(today).diff(startOfWeek, 'week') + 1;
+  // +1, becouse if difference is 0, it means that it's first week
+  return diff + 1;
 }
