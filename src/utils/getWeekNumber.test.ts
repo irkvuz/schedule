8080557e89e42ff0cd633fdd414d5e4dc75f8083
Yury Savin <yury@savin.dev>
2020-02-22 21:06:24 +0100

    Update tests for getWeekNumber

--- src/utils/getWeekNumber.test.ts
+++ src/utils/getWeekNumber.test.ts
@@ -1,24 +1,41 @@
-import moment from 'moment';
 import { getWeekNumber } from './getWeekNumber';
 
 describe('getWeekNumber() works', () => {
-  moment.locale('ru');
-
-  it('Trimester starts on Friday', () => {
-    // Same day
-    expect(getWeekNumber('2020-02-21T00:00:00', '2020-02-21')).toEqual(1);
-    // Monday next week
-    expect(getWeekNumber('2020-02-21T00:00:00', '2020-02-24')).toEqual(2);
+  it('Monday->Monday', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-01')).toEqual(1);
+  });
+  it('Monday->Friday', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-05')).toEqual(1);
+  });
+  it('Monday->Sunday', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-07')).toEqual(1);
+  });
+  it('Monday->Monday+1', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-08')).toEqual(2);
+  });
+  it('Monday->Sunday+1', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-14')).toEqual(2);
+  });
+  it('Monday->Monday+2', () => {
+    expect(getWeekNumber('2018-01-01T00:00:00', '2018-01-15')).toEqual(3);
+  });
+  it('Tuesday->Tuesday', () => {
+    expect(getWeekNumber('2018-01-02T00:00:00', '2018-01-02')).toEqual(1);
+  });
+  it('Tuesday->Sunday', () => {
+    expect(getWeekNumber('2018-01-02T00:00:00', '2018-01-02')).toEqual(1);
+  });
+  it('Tuesday->Monday+1', () => {
+    expect(getWeekNumber('2018-01-02T00:00:00', '2018-01-08')).toEqual(2);
+  });
+  it('Friday->Friday', () => {
+    expect(getWeekNumber('2018-01-05T00:00:00', '2018-01-05')).toEqual(1);
+  });
+  it('Friday->Monday+1', () => {
+    expect(getWeekNumber('2018-01-05T00:00:00', '2018-01-08')).toEqual(2);
   });
 
-  it('Trimester starts on Monday', () => {
-    // Sunday same week
-    expect(getWeekNumber('2020-02-17T00:00:00', '2020-02-23')).toEqual(1);
-    // Monday next week
-    expect(getWeekNumber('2020-02-17T00:00:00', '2020-02-24')).toEqual(2);
-    // Friday next Week
-    expect(getWeekNumber('2020-02-17T00:00:00', '2020-02-28')).toEqual(2);
-    // Monday after 2 weeks
-    expect(getWeekNumber('2020-02-17T00:00:00', '2020-03-02')).toEqual(3);
+  it('Friday->Friday+1', () => {
+    expect(getWeekNumber('2018-01-05T00:00:00', '2018-01-12')).toEqual(2);
   });
 });
