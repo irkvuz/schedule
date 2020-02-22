d42b807fcc388d888312e155fbad710736220899
Yury Savin <yury@savin.dev>
2020-02-22 13:20:27 +0100

    Revert "Reduce icons"
    This reverts commit 323d4a3e4682664c6645ec4c0bdddd289c5b9fd1.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const { override, fixBabelImports } = require('customize-cra');
 
 module.exports = override(
@@ -6,15 +5,5 @@ module.exports = override(
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
-  }),
-  // used to minimise bundle size by 500KB
-  function(config, env) {
-    const alias = config.resolve.alias || {};
-    alias['@ant-design/icons/lib/dist$'] = path.resolve(
-      __dirname,
-      './src/icons.js'
-    );
-    config.resolve.alias = alias;
-    return config;
-  }
+  })
 );
--- src/icons.js
+++
@@ -1,6 +0,0 @@
-export {
-  default as SearchOutline,
-} from '@ant-design/icons/lib/outline/SearchOutline';
-export {
-  default as DownOutline,
-} from '@ant-design/icons/lib/outline/DownOutline';
