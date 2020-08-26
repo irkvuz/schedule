d79f368af3c92ef19c15d0600134b5b095f82cc9
Yury Savin <yury@savin.dev>
2020-08-26 12:54:09 +0200

    Fix test

--- src/Lists.test.tsx
+++ src/Lists.test.tsx
@@ -9,7 +9,7 @@ afterEach(cleanup);
 describe('<FacultiesList/>', () => {
   const faculties: IFacultyOld[] = require('../public/data/faculties.json');
 
-  test('Renders all faculties', () => {
+  it('Should render all faculties', () => {
     const wrapper = render(
       <MemoryRouter>
         <UniversalListPL loading={true} title="Title" items={faculties} />
@@ -17,9 +17,16 @@ describe('<FacultiesList/>', () => {
     );
     let ul = wrapper.container.getElementsByTagName('ul')[0];
     expect(ul.childElementCount).toBe(faculties.length);
-
+  });
+  it('Should render only faculties matching search', () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <UniversalListPL loading={true} title="Title" items={faculties} />
+      </MemoryRouter>
+    );
+    let ul = wrapper.container.getElementsByTagName('ul')[0];
     const input = wrapper.getByTestId('input-search');
-    fireEvent.change(input, { target: { value: 'Информатики' } });
+    fireEvent.change(input, { target: { value: 'Управления' } });
     // wrapper.debug();
     ul = wrapper.container.getElementsByTagName('ul')[0];
     expect(ul.childElementCount).toBe(1);
