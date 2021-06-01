5150ce94970891bbd338f5641be00dc4a0e3b615
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-01 11:20:03 +0000

    Deploy irkvuz/schedule to github.com/irkvuz/schedule.git:gh-pages

---
+++ precache-manifest.97e43814939f33f3a2ea86a91e37be75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74160400bf6ab9befa171580833df9f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf98544d563f24a90f1f",
+    "url": "/static/css/2.c52b6ba4.chunk.css"
+  },
+  {
+    "revision": "268bc88052264fe59e16",
+    "url": "/static/css/main.419a33f8.chunk.css"
+  },
+  {
+    "revision": "bf98544d563f24a90f1f",
+    "url": "/static/js/2.58a9e061.chunk.js"
+  },
+  {
+    "revision": "1373b702bbc41f067fcb4052c855366f",
+    "url": "/static/js/2.58a9e061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e4393ae2aef7359374",
+    "url": "/static/js/3.eafb5e63.chunk.js"
+  },
+  {
+    "revision": "268bc88052264fe59e16",
+    "url": "/static/js/main.5701edcf.chunk.js"
+  },
+  {
+    "revision": "b588df54512466255207",
+    "url": "/static/js/runtime-main.4e7dc374.js"
+  }
+]);
