8fee1f5eda909201f817c1fe83ad3be3dc282d9d
Yury Savin <yury@savin.dev>
2021-12-10 00:02:13 +0100

    test: add today for tabsweekdays

--- src/Schedule/TabsWeekDays.test.tsx
+++ src/Schedule/TabsWeekDays.test.tsx
@@ -5,9 +5,15 @@ import TabsWeekDays from './TabsWeekDays';
 afterEach(cleanup);
 
 describe('<TabsWeekDays/>', () => {
+  const today = new Date('2021-12-09');
   test('Return spinner if `loading` is true', () => {
     const wrapper = render(
-      <TabsWeekDays loading={true} schedule={[]} week_number={1} />
+      <TabsWeekDays
+        loading={true}
+        schedule={[]}
+        week_number={1}
+        today={today}
+      />
     );
     const loadingSpinner = wrapper.getByTestId('loading-spinner');
     expect(loadingSpinner.className).toContain('ant-spin');
@@ -16,7 +22,12 @@ describe('<TabsWeekDays/>', () => {
   test('Parity switcher switch parity between `odd` and `even`', () => {
     const schedule = require('../../public/data/schedule/1069/17854.json');
     const wrapper = render(
-      <TabsWeekDays loading={false} schedule={schedule} week_number={1} />
+      <TabsWeekDays
+        loading={false}
+        schedule={schedule}
+        week_number={1}
+        today={today}
+      />
     );
     // wrapper.debug();
     const weekParitySwitcher = wrapper.getByTestId('week-parity-switcher');
