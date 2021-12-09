ae8e2338960de0126fc64d01c4087191b19549f1
baitun <baitun@users.noreply.github.com>
2021-12-09 19:57:42 +0000

    deploy: ea4563a3d2d54255ca04d1d5e754a68f1abf45c8

---
+++ precache-manifest.c17143a026a335f7b35629dbd33b5e80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1c78ba895ef540436b3714b12de658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b38bcf6ce3d6d8a6bf5",
+    "url": "/static/css/2.c52b6ba4.chunk.css"
+  },
+  {
+    "revision": "d4cbe1b42c7f94cee8f6",
+    "url": "/static/css/main.419a33f8.chunk.css"
+  },
+  {
+    "revision": "0b38bcf6ce3d6d8a6bf5",
+    "url": "/static/js/2.90643481.chunk.js"
+  },
+  {
+    "revision": "1373b702bbc41f067fcb4052c855366f",
+    "url": "/static/js/2.90643481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652c64c673bea47f9e1f",
+    "url": "/static/js/3.3b0a4882.chunk.js"
+  },
+  {
+    "revision": "d4cbe1b42c7f94cee8f6",
+    "url": "/static/js/main.04f3a247.chunk.js"
+  },
+  {
+    "revision": "e92824ab80d1ff6b08c9",
+    "url": "/static/js/runtime-main.0ec8465f.js"
+  }
+]);
