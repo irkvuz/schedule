62569106c433b5ae4573a8f09459de24f4dcf47b
Yury Savin <yury@savin.dev>
2022-01-24 20:53:32 +0100

    feat: use native yandex metrika

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import ru_RU from 'antd/lib/locale/ru_RU';
 import { Action, createBrowserHistory, Location } from 'history';
 import React from 'react';
 import { Router } from 'react-router-dom';
-import ym, { YMInitializer } from 'react-yandex-metrika';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 import { Main } from './Main';
-import { reachGoal } from './utils/customYandexMetrika';
-
-const isProduction = process.env.NODE_ENV === 'production';
+import { hit, reachGoal } from './utils/customYandexMetrika';
 
 /** Detects if device is on iOS */
 const isIos = () => {
@@ -22,33 +19,21 @@ const isInStandaloneMode = (): boolean => {
 };
 
 if (isIos() && isInStandaloneMode()) {
-  setTimeout(()=>{
-     reachGoal('standalone');
-  }, 5*1000);
+  setTimeout(() => {
+    reachGoal('standalone');
+  }, 5 * 1000);
 }
 
 const browserHistory = createBrowserHistory();
 
 browserHistory.listen((location: Location, action: Action) => {
   const url = location.pathname + location.search + location.hash;
-  if (isProduction) ym('hit', url);
+  hit(url);
 });
 
 export default function App() {
   return (
     <ErrorBoundary>
-      {/* счетчик нужно инициализировать в начале а не в конце, иначе возникает ошибка при редиректе */}
-      {isProduction && (
-        <YMInitializer
-          accounts={[50381566]}
-          options={{
-            clickmap: true,
-            trackLinks: true,
-            webvisor: true,
-          }}
-          version="2"
-        />
-      )}
       <ConfigProvider locale={ru_RU}>
         <Router history={browserHistory}>
           <Main />
