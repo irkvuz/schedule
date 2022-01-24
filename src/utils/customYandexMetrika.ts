fb66bb858916a8f091cde16203ad6aaa768559c3
Yury Savin <yury@savin.dev>
2022-01-24 20:53:32 +0100

    feat: use native yandex metrika

--- src/utils/customYandexMetrika.ts
+++ src/utils/customYandexMetrika.ts
@@ -1,9 +1,35 @@
-import ym from 'react-yandex-metrika';
-
-export function reachGoal(identifier: String) {
-  if (process.env.NODE_ENV === 'production') {
-    ym('reachGoal', identifier);
-  } else if (process.env.NODE_ENV !== 'test') {
-    console.log('[Yandex Metrika] reachGoal', identifier, process.env.NODE_ENV);
+declare global {
+  interface Window {
+    ym: (id: number, action: 'hit' | 'reachGoal', args: any) => void;
   }
+}
+
+const YM_ID = Number(process.env.YANDEX_METRIKA_IDENTIFIER);
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export function reachGoal(identifier: string) {
+  if(!isProduction) {
+    console.log('[Yandex Metrika] reachGoal', identifier);
+  }
+
+  if (isProduction && typeof window.ym !== 'function') {
+    console.error('Tracking is missing');
+    return;
+  }
+
+  window.ym(YM_ID, 'reachGoal', identifier)
+}
+
+export function hit(url: string) {
+  if(!isProduction) {
+    console.log('[Yandex Metrika] hit', url);
+  }
+
+  if (isProduction && typeof window.ym !== 'function') {
+    console.error('Tracking is missing');
+    return;
+  }
+
+  window.ym(YM_ID, 'hit', url);
 }
