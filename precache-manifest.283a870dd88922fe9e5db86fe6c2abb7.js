fc9232ffa626ce61c5f0329e4e5afe8c4235ad44
baitun <baitun@users.noreply.github.com>
2022-02-10 20:25:59 +0000

    deploy: 9fee510a4f05849009d8a35189ee39d36b0a3127

--- precache-manifest.283a870dd88922fe9e5db86fe6c2abb7.js
+++ precache-manifest.283a870dd88922fe9e5db86fe6c2abb7.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3e6a6a54831d9b9fa034a6035626d78",
+    "revision": "2bcdf143a9a1dc022522594411e19074",
     "url": "/index.html"
   },
   {
-    "revision": "b0e71fc894304a8389f1",
+    "revision": "995ba83baf964c93443d",
     "url": "/static/css/2.c28ac8f2.chunk.css"
   },
   {
-    "revision": "7995b97785403093dc06",
+    "revision": "f80b88ae32bb88154aad",
     "url": "/static/css/main.a324438f.chunk.css"
   },
   {
-    "revision": "b0e71fc894304a8389f1",
-    "url": "/static/js/2.929ccf74.chunk.js"
+    "revision": "995ba83baf964c93443d",
+    "url": "/static/js/2.498d3fe4.chunk.js"
   },
   {
     "revision": "7f7a51412ae3b009bfc6398406176c2c",
-    "url": "/static/js/2.929ccf74.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.498d3fe4.chunk.js.LICENSE.txt"
   },
   {
     "revision": "667ed122ad9db7e65d96",
     "url": "/static/js/3.dd14a62c.chunk.js"
   },
   {
-    "revision": "7995b97785403093dc06",
-    "url": "/static/js/main.24e80938.chunk.js"
+    "revision": "f80b88ae32bb88154aad",
+    "url": "/static/js/main.1163a00b.chunk.js"
   },
   {
     "revision": "17c94e43caee6ea568d0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44aa85bb5d13cd3a0627fd5a588fe51b.js"
+  "/precache-manifest.283a870dd88922fe9e5db86fe6c2abb7.js"
 );
 
 self.addEventListener('message', (event) => {
