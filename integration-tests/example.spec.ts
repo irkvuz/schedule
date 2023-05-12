9e0c766f1a191b69e1766652d7afef48b2d1363f
Yury Savin <yury@savin.dev>
2023-05-12 19:54:44 +0200

    Fix tests

--- integration-tests/example.spec.ts
+++ integration-tests/example.spec.ts
@@ -8,12 +8,3 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Расписание БГУ/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
