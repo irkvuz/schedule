fdcfa9d986eb4fa7b834cabe22dbfc19114a5fc8
Yury Savin <yury@savin.dev>
2023-05-12 19:43:44 +0200

    move to root folder

--- integration-tests/example.spec.ts
+++ integration-tests/example.spec.ts
@@ -1,10 +1,11 @@
+/* eslint-disable jest/no-done-callback */
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://bgu.irkvuz.ru/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Расписание БГУ/);
 });
 
 test('get started link', async ({ page }) => {
