2920b8f8e80d4051367c8ff67909a0417fab155b
Yury Savin <yury@savin.dev>
2023-07-05 20:49:35 +0200

    test: Fix e2e tests

--- integration-tests/full-cycle.integration.spec.ts
+++ integration-tests/full-cycle.integration.spec.ts
@@ -4,13 +4,13 @@ import { test, expect } from '@playwright/test';
 test('Full jerney for М-21-1', async ({ page }) => {
   await page.goto('https://bgu.irkvuz.ru/');
   expect(page).toHaveURL('https://bgu.irkvuz.ru/faculties');
-  await page.getByTestId('input-search').fill('Институт управления');
-  await page.getByRole('link', { name: 'Институт управления и финансов' }).click();
-  expect(page).toHaveURL('https://bgu.irkvuz.ru/205/');
+  await page.getByTestId('input-search').fill('Колледж');
+  await page.getByRole('link', { name: 'Колледж Байкальского государственного университета' }).click();
+  expect(page).toHaveURL('https://bgu.irkvuz.ru/211/');
   
-  await page.getByTestId('input-search').fill('М-');
-  await page.getByRole('link', { name: 'М-21-1' }).click();
-  expect(page).toHaveURL('https://bgu.irkvuz.ru/205/31019/');
+  await page.getByTestId('input-search').fill('БД');
+  await page.getByRole('link', { name: 'БД(9)-22-1' }).click();
+  expect(page).toHaveURL('https://bgu.irkvuz.ru/211/31899/');
   
   await page.getByTestId('week-parity-switcher').click();
   expect(page.locator('.ant-message-notice-content')).toBeVisible();
